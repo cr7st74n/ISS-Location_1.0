e67c45e01fd28006e20503b8a7ba9a7b943c7422
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-03-18 22:08:35 -0400

    adding the icon

--- app/app.js
+++ app/app.js
@@ -7,29 +7,53 @@ setInterval(()=>{
     time_ISS.innerHTML = date
 }, 1000)
 
+//get tge ISS data every 10 sec
+const update =10000
+let ISSmarker
+
+get_ISSData()
+setInterval(get_ISSData,update);
+
 //get all the elememt to display the information 
 const isslat = document.querySelector("#ISS-lat");
 const isslong = document.querySelector("#ISS-long");
 
-// Fetch the information to added to our p elements.
-fetch(url).then((res)=>{
-    return res.json()
-}).then((issData)=>{
-    console.log(issData);
-    // call lat and long from our json data.
-    isslat.innerHTML = issData.latitude;
-    isslong.innerHTML = issData.longitude;
- }).catch((err)=>{
-    console.log('ERROR in the fetch', err);
- })
-
- //map part 
-let zoomLevel = 4;
-let US_coordinates = [40.0379411,-98.5021668,5]
 // Create the map 
-let map = L.map('Map-ISS').setView(US_coordinates, zoomLevel);
+let map = L.map('Map-ISS').setView([0,0], 1);
 
 // Add the tile layer - roads, streets etc. Without this, nothing to see 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copywrite">OpenStreetMap</a>',    
 }).addTo(map);
+
+
+//set icon 
+const ISSIcon =  L.icon({
+    iconUrl: "./css/icons8-space-station-60.png",
+    iconSize:     [20, 27]
+});
+
+// Fetch the information to added to our p elements.
+
+function get_ISSData(){
+
+fetch(url).then((res)=>{
+    return res.json()
+}).then((issData)=>{
+    // call lat and long from our json data.
+    isslat.innerHTML = issData.latitude;
+    isslong.innerHTML = issData.longitude;
+
+    //create the marker
+    if(!ISSmarker){
+        ISSmarker = L.marker([issData.latitude,issData.longitude],{icon:ISSIcon}).addTo(map);
+    }else {
+        ISSmarker.setLatLng([issData.latitude,issData.longitude]);
+    }
+
+ }).catch((err)=>{
+    console.log('ERROR in the fetch', err);
+ })
+
+
+}
